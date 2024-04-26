e4e792fae2125d3be853b0fb0ea520098d8e684a
Joojinsung <dev.jinsung1017@gmail.com>
2024-04-26 15:38:06 +0900

    :wrench: 기본설정, 디펜던시, 추가

--- src/main/java/com/backend/supermeproject/global/exception/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.backend.supermeproject.global.exception;
-import org.springframework.http.HttpStatus;
-
-public record ErrorResponse(
-        HttpStatus status,
-        String message
-) {
-}
-
--- src/main/java/com/backend/supermeproject/member/role/Gender.java
+++
@@ -1,4 +0,0 @@
-package com.backend.supermeproject.member.role;
-
-public enum Gender {
-}
