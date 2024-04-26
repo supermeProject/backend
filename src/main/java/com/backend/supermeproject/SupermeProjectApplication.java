b545f07398077fbca7ac174642ad05b9788678e6
Joojinsung <dev.jinsung1017@gmail.com>
2024-04-26 14:20:18 +0900

    초기세팅

---
+++ src/main/java/com/backend/supermeproject/SupermeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.backend.supermeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermeProjectApplication.class, args);
+	}
+
+}
