4a0fb8248dc662a5255e03b27965a049db58e126
Joojinsung <dev.jinsung1017@gmail.com>
2024-04-26 15:43:00 +0900

    :wrench: 기본설정, 디펜던시, 추가

---
+++ src/test/java/com/backend/supermeproject/SupermeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.supermeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
